d55ed817f951fbb4830d09342f487b1a7a3363c4
Arnab Ghosh <ghosharnab436@gmail.com>
2026-05-14 18:43:27 +0530

    ok

--- src/components/Jobs.jsx
+++ src/components/Jobs.jsx
@@ -1,31 +1,26 @@
 import React from 'react'
 
+const sortBtns = document.querySelectorAll('.job-id li');
+
+sortBtns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    sortBtns.forEach((btn) => btn.classList.remove('active'));
+    btn.classList.add('active');
+  });
+  
+});
+
 const Jobs = () => {
   return (
     <section id='jobs' className='jobs'>
       <h1>Latest Job Notifications</h1>
-      <section className="box-section">
-         <a href='Govt.jsx' className='box-link'  >
-        <div className="box">
-            <h2><u>GOVT Jobs</u></h2>
-          
-          <p>
-            Government jobs offer stable careers with secure income, fixed hours, and great benefits. They’re highly valued for long-term growth, respect, and reliability.
-
-          </p>
-        </div>
-        </a>
-
-        <a href='https://www.naukri.com/private-jobs-in-kolkata' className='box-link'>
-        <div className="box">
-          <h2><u>Private Jobs</u></h2>
-          <p>
-            Private jobs offer fast growth, dynamic work environments, and performance-based rewards. They’re ideal for building skills, gaining experience, and advancing your career quickly.
-
-          </p>
-
-        </div>
-        </a>
+      <section className="job-section">
+        <ul className='job-id'>
+          <li>Recent Jobs</li>
+          <li>Full Time</li>
+          <li>Part Time</li>
+        </ul>
+      
       </section>
     </section>
   )
